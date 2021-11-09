6cc5fa1a4566e6f837bb864d1863648bbe314c4d
Deatsilence <78795973+Sylar-Z31@users.noreply.github.com>
2021-11-10 01:10:27 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace Proje_Bilgi_Yarışması
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richtxtBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblNo = new System.Windows.Forms.Label();
+            this.lblTrue = new System.Windows.Forms.Label();
+            this.lblFalse = new System.Windows.Forms.Label();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richtxtBox
+            // 
+            this.richtxtBox.Enabled = false;
+            this.richtxtBox.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richtxtBox.Location = new System.Drawing.Point(-1, 0);
+            this.richtxtBox.Name = "richtxtBox";
+            this.richtxtBox.Size = new System.Drawing.Size(482, 148);
+            this.richtxtBox.TabIndex = 4;
+            this.richtxtBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(150)))));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(520, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Soru No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(533, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Doğru:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(532, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Yanlış:";
+            // 
+            // lblNo
+            // 
+            this.lblNo.AutoSize = true;
+            this.lblNo.ForeColor = System.Drawing.Color.White;
+            this.lblNo.Location = new System.Drawing.Point(585, 9);
+            this.lblNo.Name = "lblNo";
+            this.lblNo.Size = new System.Drawing.Size(15, 16);
+            this.lblNo.TabIndex = 8;
+            this.lblNo.Text = "0";
+            // 
+            // lblTrue
+            // 
+            this.lblTrue.AutoSize = true;
+            this.lblTrue.ForeColor = System.Drawing.Color.White;
+            this.lblTrue.Location = new System.Drawing.Point(585, 46);
+            this.lblTrue.Name = "lblTrue";
+            this.lblTrue.Size = new System.Drawing.Size(15, 16);
+            this.lblTrue.TabIndex = 9;
+            this.lblTrue.Text = "0";
+            // 
+            // lblFalse
+            // 
+            this.lblFalse.AutoSize = true;
+            this.lblFalse.ForeColor = System.Drawing.Color.White;
+            this.lblFalse.Location = new System.Drawing.Point(585, 82);
+            this.lblFalse.Name = "lblFalse";
+            this.lblFalse.Size = new System.Drawing.Size(15, 16);
+            this.lblFalse.TabIndex = 10;
+            this.lblFalse.Text = "0";
+            // 
+            // btnA
+            // 
+            this.btnA.Enabled = false;
+            this.btnA.Location = new System.Drawing.Point(30, 154);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(211, 31);
+            this.btnA.TabIndex = 11;
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.Enabled = false;
+            this.btnB.Location = new System.Drawing.Point(270, 154);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(211, 31);
+            this.btnB.TabIndex = 12;
+            this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.Enabled = false;
+            this.btnC.Location = new System.Drawing.Point(30, 191);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(211, 31);
+            this.btnC.TabIndex = 13;
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.Enabled = false;
+            this.btnD.Location = new System.Drawing.Point(270, 190);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(211, 31);
+            this.btnD.TabIndex = 14;
+            this.btnD.UseVisualStyleBackColor = true;
+            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(500, 154);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(56, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(564, 154);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(56, 67);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnNext.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnNext.Location = new System.Drawing.Point(497, 117);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(122, 31);
+            this.btnNext.TabIndex = 18;
+            this.btnNext.Text = "Başla";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(7, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 16);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(7, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 16);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "C";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(247, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 16);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "B";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(247, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 16);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "D";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(150)))));
+            this.ClientSize = new System.Drawing.Size(634, 233);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.lblFalse);
+            this.Controls.Add(this.lblTrue);
+            this.Controls.Add(this.lblNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richtxtBox);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Kim Milyoner Olmak İstemezki";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richtxtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblNo;
+        private System.Windows.Forms.Label lblTrue;
+        private System.Windows.Forms.Label lblFalse;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje_Bilgi_Yarışması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        uint soruNo = 0, dogru = 0, yanlis = 0;
+        string key;
+
+        private void btnNext_Click_1(object sender, EventArgs e)
+        {
+            btnNext.Text = "İlerle";
+            btnNext.Enabled = false;
+            soruNo++;
+            lblNo.Text = soruNo.ToString();
+
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            btnA.Enabled = true;
+            btnB.Enabled = true;
+            btnC.Enabled = true;
+            btnD.Enabled = true;
+            btnA.BackColor = Color.White;
+            btnB.BackColor = Color.White;
+            btnC.BackColor = Color.White;
+            btnD.BackColor = Color.White;
+
+
+            if (soruNo == 1)
+            {
+                richtxtBox.Text = "Türkiye'nin başkenti neresidir ?";
+
+                btnA.Text = "İstanbul";
+                btnB.Text = "Ankara";
+                btnC.Text = "İzmir";
+                btnD.Text = "Yalova";
+                key = btnB.Text;
+            }
+            if (soruNo == 2)
+            {
+                richtxtBox.Text = "Matrix serisi kaç tane filmden oluşur ?";
+
+                btnA.Text = "3";
+                btnB.Text = "4";
+                btnC.Text = "5";
+                btnD.Text = "6";
+                key = btnA.Text;
+            }
+            if (soruNo == 3)
+            {
+                richtxtBox.Text = "Haftaya dün çarşambaysa bu hafta yarın hangi gündür ?";
+
+                btnA.Text = "perşembe";
+                btnB.Text = "pazar";
+                btnC.Text = "salı";
+                btnD.Text = "cuma";
+                key = btnD.Text;
+            }
+        }
+
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnNext.Enabled = true;
+
+            if (btnA.Text == key)
+            {
+                dogru++;
+                lblTrue.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+                btnA.BackColor = Color.Green;
+            }
+            else
+            {
+                yanlis++;
+                lblFalse.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+                btnA.BackColor = Color.Red;
+
+                if (btnB.Text == key)
+                {
+                    btnB.BackColor = Color.Green;
+                }
+                else if (btnC.Text == key)
+                {
+                    btnC.BackColor = Color.Green;
+                }
+                else if (btnD.Text == key)
+                {
+                    btnD.BackColor = Color.Green;
+                }
+            }
+            if(soruNo == 3) { btnNext.Text = "Bitti !"; btnNext.Enabled = false; MessageBox.Show(soruNo + " Soruda " + dogru + " Doğru " + yanlis + " Yanlış"); }
+        }
+
+        private void btnB_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnNext.Enabled = true;
+
+            if (btnB.Text == key)
+            {
+                dogru++;
+                lblTrue.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+                btnB.BackColor = Color.Green;
+            }
+            else
+            {
+                yanlis++;
+                lblFalse.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+                btnB.BackColor = Color.Red;
+
+                if (btnA.Text == key)
+                {
+                    btnA.BackColor = Color.Green;
+                }
+                else if (btnC.Text == key)
+                {
+                    btnC.BackColor = Color.Green;
+                }
+                else if (btnD.Text == key)
+                {
+                    btnD.BackColor = Color.Green;
+                }
+
+            }
+            if (soruNo == 3) { btnNext.Text = "Bitti !"; btnNext.Enabled = false; MessageBox.Show(soruNo + " Soruda " + dogru + " Doğru " + yanlis + " Yanlış"); }
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnNext.Enabled = true;
+
+            if (btnC.Text == key)
+            {
+                dogru++;
+                lblTrue.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+                btnC.BackColor = Color.Green;
+            }
+            else
+            {
+                yanlis++;
+                lblFalse.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+                btnC.BackColor = Color.Red;
+
+                if (btnA.Text == key)
+                {
+                    btnA.BackColor = Color.Green;
+                }
+                else if (btnB.Text == key)
+                {
+                    btnB.BackColor = Color.Green;
+                }
+                else if (btnD.Text == key)
+                {
+                    btnD.BackColor = Color.Green;
+                }
+            }
+            if (soruNo == 3) { btnNext.Text = "Bitti !"; btnNext.Enabled = false; MessageBox.Show(soruNo + " Soruda " + dogru + " Doğru " + yanlis + " Yanlış"); }
+        }
+
+        private void btnD_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnNext.Enabled = true;
+
+            if (btnD.Text == key)
+            {
+                dogru++;
+                lblTrue.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+                btnD.BackColor = Color.Green;
+            }
+            else
+            {
+                yanlis++;
+                lblFalse.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+                btnD.BackColor = Color.Red;
+
+                if (btnA.Text == key)
+                {
+                    btnA.BackColor = Color.Green;
+                }
+                else if (btnB.Text == key)
+                {
+                    btnB.BackColor = Color.Green;
+                }
+                else if (btnC.Text == key)
+                {
+                    btnC.BackColor = Color.Green;
+                }
+            }
+            if (soruNo == 3) { btnNext.Text = "Bitti !"; btnNext.Enabled = false; MessageBox.Show(soruNo + " Soruda " + dogru + " Doğru " + yanlis + " Yanlış"); }
+        }
+
+
+    }
+}
